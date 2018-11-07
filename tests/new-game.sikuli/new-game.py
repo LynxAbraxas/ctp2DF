9c80cf60604382d532b082ac95191512ebff2fbf
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-07 23:12:22 +0100

    wait extra 5s because control bar appears before progressbar vanishes

--- tests/new-game.sikuli/new-game.py
+++ tests/new-game.sikuli/new-game.py
@@ -4,6 +4,7 @@ wait("ctp2start-scr.png", 100)
 click("ctp2new-game-btn.png")
 click("ctp2launch-btn.png")
 if exists("ctp2new_res.png", 100):
+    wait(5) # control bar appears before progressbar vanishes
     file = capture(SCREEN.getBounds())
     print("Saved screen as "+file)
     shutil.move(file, 'ctp2new-game.png')
