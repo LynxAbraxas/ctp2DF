a6d8aa80ba59cba5a7041d3c33a2dfa3edc00138
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-07 19:08:08 +0100

    let sikuli take the screen shot:
    http://doc.sikuli.org/screen.html#screen-setting-getting-attributes-and-information
    https://stackoverflow.com/questions/16745722/whats-the-command-to-take-a-picture-in-sikuli

--- tests/new-game.sikuli/new-game.py
+++ tests/new-game.sikuli/new-game.py
@@ -1,7 +1,12 @@
+import shutil
+
 wait("ctp2start-scr.png", 100)
 click("ctp2new-game-btn.png")
 click("ctp2launch-btn.png")
-if exists("ctp2new_res.png"):
+if exists("ctp2new_res.png", 100):
+    file = capture(SCREEN.getBounds())
+    print("Saved screen as "+file)
+    shutil.move(file, 'ctp2new-game.png')
     exit(0)
 else:
-    exit(1)
+    exit(10)
