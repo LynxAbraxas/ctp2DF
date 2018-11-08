8ffe1620b49407c3c14863192df3a2f91f56a327
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-08 15:17:00 +0100

    incr. sensitivity for OK-btn (sikuli was hitting delete-btn):
    https://sikulix-2014.readthedocs.io/en/latest/pattern.html#Pattern.similar

--- tests/load-game.sikuli/load-game.py
+++ tests/load-game.sikuli/load-game.py
@@ -4,7 +4,7 @@ wait("ctp2start-scr.png", 100)
 click("ctp2load-game-btn.png")
 click("ctp2Julius-folder.png")
 click("ctp2Julius-Roma-4000BC-file.png")
-click("ctp2OK-btn.png")
+click(Pattern("ctp2OK-btn.png").similar(0.99))
 if exists("ctp2ctr-bar.png", 100):
     waitVanish("ctp2progress-bar.png", 100) # control bar appears before progressbar vanishes
     file = capture(SCREEN.getBounds())
