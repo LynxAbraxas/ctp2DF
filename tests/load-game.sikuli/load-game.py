810043abcb3e9098598a8521835de5557f99fcf0
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-08 15:07:13 +0100

    loading saved game test

--- tests/load-game.sikuli/load-game.py
+++ tests/load-game.sikuli/load-game.py
@@ -1,13 +1,15 @@
 import shutil
 
 wait("ctp2start-scr.png", 100)
-click("ctp2new-game-btn.png")
-click("ctp2launch-btn.png")
+click("ctp2load-game-btn.png")
+click("ctp2Julius-folder.png")
+click("ctp2Julius-Roma-4000BC-file.png")
+click("ctp2OK-btn.png")
 if exists("ctp2ctr-bar.png", 100):
     waitVanish("ctp2progress-bar.png", 100) # control bar appears before progressbar vanishes
     file = capture(SCREEN.getBounds())
     print("Saved screen as "+file)
-    shutil.move(file, 'ctp2new-game.png')
+    shutil.move(file, 'ctp2load-game.png')
     exit(0)
 else:
     exit(10)
