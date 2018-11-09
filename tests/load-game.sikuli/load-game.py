b8af8dca10dd5cd0b5d91123b9120144f0df3d9c
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-09 10:15:50 +0100

    moved sikuli commands into function, called as main from IDE

--- tests/load-game.sikuli/load-game.py
+++ tests/load-game.sikuli/load-game.py
@@ -1,15 +1,19 @@
 import shutil
 
-wait("ctp2start-scr.png", 100)
-click("ctp2load-game-btn.png")
-click("ctp2Julius-folder.png")
-click("ctp2Julius-Roma-file.png")
-click(Pattern("ctp2OK-btn.png").similar(0.99))
-if exists("ctp2ctr-bar.png", 100):
-    waitVanish("ctp2progress-bar.png", 100) # control bar appears before progressbar vanishes
-    file = capture(SCREEN.getBounds())
-    print("Saved screen as "+file)
-    shutil.move(file, 'ctp2load-game.png')
-    exit(0)
-else:
-    exit(10)
+def main():
+    wait("ctp2start-scr.png", 100)
+    click("ctp2load-game-btn.png")
+    click("ctp2Julius-folder.png")
+    click("ctp2Julius-Roma-file.png")
+    click(Pattern("ctp2OK-btn.png").similar(0.99))
+    if exists("ctp2ctr-bar.png", 100):
+        waitVanish("ctp2progress-bar.png", 100) # control bar appears before progressbar vanishes
+        file = capture(SCREEN.getBounds())
+        print("Saved screen as "+file)
+        shutil.move(file, 'ctp2load-game.png')
+        exit(0)
+    else:
+        exit(10)
+
+if __name__ == "__main__":
+    main()
--- tests/start-game.sikuli/start-game.py
+++ tests/start-game.sikuli/start-game.py
@@ -1,9 +1,13 @@
 import shutil
 
-if exists("ctp2start-scr.png", 100):
-    file = capture(SCREEN.getBounds())
-    print("Saved screen as "+file)
-    shutil.move(file, 'ctp2start.png')
-    exit(0)
-else:
-    exit(10)
+def main():
+    if exists("ctp2start-scr.png", 100):
+        file = capture(SCREEN.getBounds())
+        print("Saved screen as "+file)
+        shutil.move(file, 'ctp2start.png')
+        exit(0)
+    else:
+        exit(10)
+
+if __name__ == "__main__":
+    main()
