3eea2f48d10b945626fbb48a1df2a0d83414ee40
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-17 10:18:40 +0100

    test: fail if screen shot does not contain pattern:
    in case program exited between waitVanish and capture
    needs Finder, cannot use Pattern(file).find("ctp2ctr-bar.png") as for screen: http://doc.sikuli.org/region.html#exception-findfailed

--- tests/new-game.sikuli/new-game.py
+++ tests/new-game.sikuli/new-game.py
@@ -16,13 +16,19 @@ def main():
     if exists("ctp2ctr-bar.png", 100):
         if waitVanish("ctp2progress-bar.png", 100): # control bar appears before progressbar vanishes
             file = capture(SCREEN.getBounds())
-            print("Saved screen as "+file)
-            shutil.move(file, 'ctp2new-game.png')
-            exit(0)
+            if file:
+                f= Finder(file) # http://doc.sikuli.org/finder.html
+                f.find("ctp2ctr-bar.png")
+                if not f.hasNext():
+                    print("Pattern not found in screen shot: " + file)
+                    exit(30)
+                shutil.move(file, 'ctp2new-game.png')
+                exit(0)
         else:
             exit(20)
     else:
         exit(10)
+    exit(99)
 
 if __name__ == "__main__":
     main()
