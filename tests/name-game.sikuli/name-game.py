11213337c99fa5851e09fc5f6c5176ae906e9c89
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-28 19:17:32 +0100

    TEST: text field needs click now to have the keyboard focus for text entry

--- tests/name-game.sikuli/name-game.py
+++ tests/name-game.sikuli/name-game.py
@@ -16,9 +16,11 @@ def main():
     wait("ctp2start-scr.png", 100)
     click("ctp2new-game-btn.png")
     wait("ctp2launch-btn.png", 100) # wait until load game window is visible
+    click(Pattern("ctp2empty-leader-name-field_end.png").similar(0.95), 10) # (non-empty) text field needs click to get focus
+    hover(getLastMatch().getTarget().offset(0, 100)) # move mouse out of the text field for later matching
     Settings.TypeDelay = 0.1; # ctp2-SDL needs some more time
     count= 0
-    while not exists(Pattern("ctp2empty-leader-name-field.png").similar(0.95), 0.01) and count < 1024:
+    while not exists(Pattern("ctp2empty-leader-name-field.png").similar(0.95), 0.01) and count < 1024: # high similarity to ensure empty text field
         type(Key.BACKSPACE) # hit backspace until name-field is empty
         count= count + 1
     type(leaderName)
