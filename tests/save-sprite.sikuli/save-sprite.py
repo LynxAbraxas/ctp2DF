fd13122318aed90b07c2be10e7c2132bc6127080
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-11-28 23:41:36 +0100

    TEST: sprite saving

--- tests/save-sprite.sikuli/save-sprite.py
+++ tests/save-sprite.sikuli/save-sprite.py
@@ -12,7 +12,7 @@ bsfn= os.path.splitext(os.path.basename(fn))[0]
 addImagePath(path)
 
 def main():
-    spriteFName= "gg006.spr" # crab
+    spriteFName= "test.spr"
     wait("ctp2start-scr.png", 100) # still works with sprite button shown
     click("ctp2sprite-test-btn.png")
     wait(10) # progress bar does not appear at once
@@ -21,22 +21,12 @@ def main():
         click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.95).targetOffset(0, -10), 100) # text field needs click to get focus
         Settings.TypeDelay = 0.1; # ctp2-SDL needs some more time
         type(spriteFName)
-        click("ctp2sprite-load-btn.png")
+        click("ctp2sprite-save-btn.png")
         wait(10)
-        if exists("ctp2sprite-crab-start-img.png", 100):
-            file = capture(SCREEN.getBounds())
-            if file:
-                f= Finder(file) # http://doc.sikuli.org/finder.html
-                f.find("ctp2sprite-crab-start-img.png")
-                if not f.hasNext():
-                    print("Pattern not found in screen shot: " + file)
-                    exit(30)
-                shutil.move(file, bsfn + '.png')
-                exit(0)
-        else:
-            file = capture(SCREEN.getBounds())
-            shutil.move(file, bsfn + '_.png')
-            exit(10)
+        file = capture(SCREEN.getBounds())
+        if file:
+            shutil.move(file, bsfn + '.png')
+            exit(0)
     else:
         exit(20)
     exit(99)
