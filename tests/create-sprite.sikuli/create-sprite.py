d3e1d7c3d03d406aa57c8ed4d335fd4921c7653f
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-12-01 22:59:30 +0100

    TEST: first import from TXT then save to SPR

--- tests/create-sprite.sikuli/create-sprite.py
+++ tests/create-sprite.sikuli/create-sprite.py
@@ -12,7 +12,7 @@ bsfn= os.path.splitext(os.path.basename(fn))[0]
 addImagePath(path)
 
 def main():
-    spriteFName= "GG100.txt"
+    spriteFName= "GG100"
     wait("ctp2start-scr.png", 100) # still works with sprite button shown
     click("ctp2sprite-test-btn.png")
     wait(10) # progress bar does not appear at once
@@ -20,9 +20,14 @@ def main():
         wait(10) # some more time
         click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.95).targetOffset(0, -10), 100) # text field needs click to get focus
         Settings.TypeDelay = 0.1; # ctp2-SDL needs some more time
-        type(spriteFName)
+        type(spriteFName + '.txt') # import from TXT and image series
         click("ctp2sprite-load-btn.png")
         wait(10)
+        count= 0
+        while not exists(Pattern("ctp2sprite-sprite-name-field.png").similar(0.95), 0.01) and count < 100: # high similarity to ensure empty text field
+            type(Key.BACKSPACE) # hit backspace until name-field is empty
+            count= count + 1
+        type(spriteFName + '.spr') # save to SPR
         click("ctp2sprite-save-btn.png")
         wait(10)
         file = capture(SCREEN.getBounds())
