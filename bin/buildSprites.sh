1631ab8b33141f612a85c5f4ccf8d76d49b307ca
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-12-07 22:45:19 +0100

    map new sprite files directly from newSprites/

--- bin/buildSprites.sh
+++ bin/buildSprites.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p newSprites/
+
 SPRITES=$( ls -1 sprites/???.blend | grep -o '[0-9]\{3\}' )
 for RES_FILE in $SPRITES ; do
     echo "Rendering ${RES_FILE}"
@@ -16,13 +18,13 @@ for RES_FILE in $SPRITES ; do
 
     awk -f sprites/bin/spriteFileGen.awk -v nf=$(ls -1 sprites/${RES_FILE}/*A*.tif | wc -l) > GG${RES_FILE}.txt
 
-    touch GG${RES_FILE}.spr # file must exist for docker bind of file
-    chmod a+rw GG${RES_FILE}* # user inside docker must have write permission!
+    touch newSprites/GG${RES_FILE}.spr # file must exist for docker bind of file
+    chmod a+rw GG${RES_FILE}.txt newSprites/GG${RES_FILE}.spr # user inside docker must have write permission!
 
     ## append sprite series folder, import TXT and export SPR to DOCKER_PARAMS
     DOCKER_PARAMS="${DOCKER_PARAMS} -v $(pwd)/sprites/${RES_FILE}/:/opt/ctp2/ctp2_program/ctp/${RES_FILE}/ "
     DOCKER_PARAMS="${DOCKER_PARAMS} -v $(pwd)/GG${RES_FILE}.txt:/opt/ctp2/ctp2_data/default/graphics/sprites/GG${RES_FILE}.txt "
-    DOCKER_PARAMS="${DOCKER_PARAMS} -v $(pwd)/GG${RES_FILE}.spr:/opt/ctp2/ctp2_data/default/graphics/sprites/GG${RES_FILE}_.spr "
+    DOCKER_PARAMS="${DOCKER_PARAMS} -v $(pwd)/newSprites/GG${RES_FILE}.spr:/opt/ctp2/ctp2_data/default/graphics/sprites/GG${RES_FILE}_.spr "
 
     echo "${RES_FILE} done."
 done
@@ -45,10 +47,7 @@ java -cp "/opt/sikulixapi.jar:/opt/jython-standalone-2.7.1.jar" org.python.util.
 
 kill $pidD
 
-mkdir -p newSprites/
-mv GG*.spr newSprites/
-
-ls newSprites/
+ls newSprites/ || true
 
 ## rename new sprites to the corresponding case used in ctp2 or ctp2CD (expecting ctp2CD to equal ctp2 in case of overlap)
 cd newSprites/ &&
@@ -60,4 +59,4 @@ cd newSprites/ &&
 		| sort -f) \
 	| xargs -n 2  mv -n
 
-ls newSprites/
+ls -la
