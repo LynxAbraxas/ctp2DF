a8d386163e21a012177fd2b6c5b1b7353820f6a3
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2018-12-08 12:28:06 +0100

    click with delay (not working so far)

--- tests/export-sprite.sikuli/export-sprite.py
+++ tests/export-sprite.sikuli/export-sprite.py
@@ -11,17 +11,30 @@ path= os.path.dirname(os.path.abspath(fn))
 bsfn= os.path.splitext(os.path.basename(fn))[0]
 addImagePath(path)
 
+def Click(pattern, delay= 1, timeout= None, modifiers= None):
+    if not timeout:
+        timeout= getAutoWaitTimeout()
+    
+    wait(pattern)
+    if getLastMatch():
+        hover(getLastMatch().getTarget())
+        wait(delay)
+        click(Mouse.at(), modifiers)
+        wait(delay)
+    else:
+        exit(200)
+
 def main():
     spriteFName= bsfn + ".txt"
     wait("ctp2start-scr.png", 100) # still works with sprite button shown
-    click("ctp2sprite-test-btn.png")
+    Click("ctp2sprite-test-btn.png")
     wait(10) # progress bar does not appear at once
     if waitVanish(Pattern("ctp2progress-bar.png").similar(0.95), 100): # wait until progressbar vanishes
         wait(10) # some more time
-        click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.95).targetOffset(0, -10)) # text field needs click to get focus
+        Click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.95).targetOffset(0, -10)) # text field needs click to get focus
         Settings.TypeDelay = 0.1; # ctp2-SDL needs some more time
         type(spriteFName)
-        click("ctp2sprite-save-btn.png")
+        Click("ctp2sprite-save-btn.png")
         wait(10)
         file = capture(SCREEN.getBounds())
         if file:
