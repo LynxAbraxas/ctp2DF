f891dd920f6aaf0078817d8804ae1e9e0124d08c
Lynx Abraxas <LynxAbraxas@users.noreply.github.com>
2019-02-01 14:12:30 +0100

    create-sprite: check if SPR exists and has non-zero size, try 3 times max

--- tests/create-sprite.sikuli/create-sprite.py
+++ tests/create-sprite.sikuli/create-sprite.py
@@ -36,38 +36,44 @@ def main():
         region= Region(match.getX()+3, match.getY()+3, 70, 17) # regtion for OCR
         print sys.argv[1:]
         for spriteNumber in sys.argv[1:]:
-            spriteFName= "GG" + spriteNumber
-            print("Processing " + spriteFName)
-            click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.7).targetOffset(0, -10)) # text field needs click to get focus
-            click(Mouse.at())
-            hover(getLastMatch().getTarget().offset(0, -100)) # move mouse out of the text field for later matching
-            count= 0
-            while not exists(Pattern("ctp2sprite-sprite-name-field.png").similar(0.98), 0.01) and count < 100: # high similarity to ensure empty text field
-                type(Key.BACKSPACE) # hit backspace until name-field is empty
-                count= count + 1
-            type(spriteFName + '.txt') # import from TXT and image series
-            checkText(spriteFName + '.tx', region) # excluding last char due to OCR of text-cursor
-            click("ctp2sprite-load-btn.png")
-            click(Mouse.at())
-            wait(10)
-            file = capture(SCREEN.getBounds())
-            if file:
-                shutil.move(file, bsfn + '_.png')
-            click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.7).targetOffset(0, -10)) # text field needs click to get focus
-            click(Mouse.at())
-            hover(getLastMatch().getTarget().offset(0, -100)) # move mouse out of the text field for later matching
-            count= 0
-            while not exists(Pattern("ctp2sprite-sprite-name-field.png").similar(0.98), 0.01) and count < 100: # high similarity to ensure empty text field
-                type(Key.BACKSPACE) # hit backspace until name-field is empty
-                count= count + 1
-            type(spriteFName + '_.spr') # save to SPR
-            checkText(spriteFName + '_.sp', region) # region stays the same
-            click("ctp2sprite-save-btn.png")
-            # 2nd click causes segfault: click(Mouse.at())
-            wait(10)
-            file = capture(SCREEN.getBounds())
-            if file:
-                shutil.move(file, bsfn + '.png')
+            success= False
+            tries= 1
+            while not success and tries < 4:
+                spriteFName= "GG" + spriteNumber
+                print("Processing " + spriteFName + " try: ", tries)
+                click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.7).targetOffset(0, -10)) # text field needs click to get focus
+                click(Mouse.at())
+                hover(getLastMatch().getTarget().offset(0, -100)) # move mouse out of the text field for later matching
+                count= 0
+                while not exists(Pattern("ctp2sprite-sprite-name-field.png").similar(0.98), 0.01) and count < 100: # high similarity to ensure empty text field
+                    type(Key.BACKSPACE) # hit backspace until name-field is empty
+                    count= count + 1
+                type(spriteFName + '.txt') # import from TXT and image series
+                checkText(spriteFName + '.tx', region) # excluding last char due to OCR of text-cursor
+                click("ctp2sprite-load-btn.png")
+                click(Mouse.at())
+                wait(10)
+                file = capture(SCREEN.getBounds())
+                if file:
+                    shutil.move(file, bsfn + '_.png')
+                click(Pattern("ctp2sprite-sprite-name-field.png").similar(0.7).targetOffset(0, -10)) # text field needs click to get focus
+                click(Mouse.at())
+                hover(getLastMatch().getTarget().offset(0, -100)) # move mouse out of the text field for later matching
+                count= 0
+                while not exists(Pattern("ctp2sprite-sprite-name-field.png").similar(0.98), 0.01) and count < 100: # high similarity to ensure empty text field
+                    type(Key.BACKSPACE) # hit backspace until name-field is empty
+                    count= count + 1
+                type(spriteFName + '_.spr') # save to SPR
+                checkText(spriteFName + '_.sp', region) # region stays the same
+                click("ctp2sprite-save-btn.png")
+                # 2nd click causes segfault: click(Mouse.at())
+                wait(10)
+                file = capture(SCREEN.getBounds())
+                if file:
+                    shutil.move(file, bsfn + '.png')
+                fpath= os.path.abspath("/opt/ctp2/ctp2_data/default/graphics/sprites/" + spriteFName + '_.spr')
+                success= os.path.isfile(fpath) and os.path.getsize(fpath) > 0
+                tries+= 1
     else:
         exit(20)
     exit(0)
